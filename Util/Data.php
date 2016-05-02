c874e38b38d6597d5da5a957d6a11da8eecf9616
Andras Ratz <ratz.andras86@gmail.com>
2016-05-02 16:02:57 +0200

    #CE-697 put back milestone data

--- Util/Data.php
+++ Util/Data.php
@@ -125,7 +125,7 @@ class Data
             $this->milestonesData .= '},';
         }
 
-        return null;
+        return rtrim($this->milestonesData, ',');
     }
 
     public function getMilestonesMarkings($campaign){
